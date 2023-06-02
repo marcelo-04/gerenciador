536d868d05d6a729191e9173d67feef744b607e8
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-06-02 16:20:33 -0300

    Trabalhando apenas com POST.

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -11,10 +11,12 @@ import java.io.PrintWriter;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		System.out.println("Cadastrando nova empresa!");
+		
 		String nomeEmpresa = request.getParameter("nome");
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa "+ nomeEmpresa +" cadastrada com sucesso!</body></html>");
 	}
